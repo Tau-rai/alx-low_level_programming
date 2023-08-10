7bc6c321d699ad03f33298a00b1f32390e3999e6
Taurai <taumasaire@gmail.com>
2023-08-10 15:52:56 +0200

    sizes correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,14 +11,14 @@ int main(void)
 	char my_character;
 	int my_integer;
 	long int my_long_integer;
-	long long int my_long_long_integer;
+	long long int mylong_long_integer;
 	float my_float;
 
-	printf("Size of a char: %lu\n", sizeof(my_character));
-	printf("Size of an int: %lu\n", sizeof(my_integer));
-	printf("Size of a long int: %lu\n", sizeof(my_long_integer));
-	printf("Size of a long long int: %lu\n", sizeof(my_long_long_integer));
-	printf("Size of a float: %lu\n", sizeof(my_float));
+	printf("Size of a char: %lu byte(s)\n", sizeof(my_character));
+	printf("Size of an int: %lu byte(s)\n", sizeof(my_integer));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(my_long_integer));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(mylong_long_integer));
+	printf("Size of a float: %lu byte(s)\n", sizeof(my_float));
 
 	return (0);
 }
