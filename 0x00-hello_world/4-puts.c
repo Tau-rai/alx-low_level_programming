838244e604febf2a591ee2fc783a4c54fead7f55
Taurai <taumasaire@gmail.com>
2023-08-10 13:54:32 +0200

    using the puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/*string initialization*/
+	char my_string[] = "Programming is like building a multilingual puzzle\n";
+
+	puts(my_string);
+
+	return (0);
+}
