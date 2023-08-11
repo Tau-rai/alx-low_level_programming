cdb6df5db1ba440e3e09131f03fa077aba1f1dbe
Taurai <taumasaire@gmail.com>
2023-08-11 15:43:08 +0200

    numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x = 0;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar(x + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}
