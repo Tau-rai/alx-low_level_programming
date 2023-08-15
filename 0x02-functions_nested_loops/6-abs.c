9c812cf76fa83f66d874fbe4595c059ee6871e30
Taurai <taumasaire@gmail.com>
2023-08-15 16:12:17 +0200

    absolute integers

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute integer of a number
+ *@int: represent the integer to be computed into an absolute integer
+ * Return: Always 0.
+ */
+
+int _abs(int)
+{
+	/*initialize an integer num*/
+	int num;
+
+	if (num < 0)
+	{
+		num = -m;
+	}
+	return (0);
+}
+
+
