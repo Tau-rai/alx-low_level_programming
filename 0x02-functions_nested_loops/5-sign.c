b56ece20cffe982080ea3fc5107bc2d7b9739995
Taurai <taumasaire@gmail.com>
2023-08-15 15:48:43 +0200

    arithmatic operators

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - checks if a number is positive , negative or zero
+ * @n: is the integer to be checked
+ *
+ * Return: 1 (Success)
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	_putchar('\n');
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	_putchar('\n');
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
