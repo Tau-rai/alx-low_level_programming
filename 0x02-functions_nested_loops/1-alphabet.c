36b549a046c483e11f6cd6d460e9a02277b5d3ae
Taurai <taumasaire@gmail.com>
2023-08-15 12:33:53 +0200

    lower alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+	return (0);
+}
