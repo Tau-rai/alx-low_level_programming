2ab5bb4b8878e519fa13444ea21b5c018adb4fd3
Taurai <taumasaire@gmail.com>
2023-08-15 12:20:36 +0200

    putc

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include "main.h"
 
 /**
@@ -6,15 +7,8 @@
  * Return: Always 0 (Success)
  */
 
-void main(void)
+int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	write(1, "_putchar\n", 9);
+	return (0);
 }
