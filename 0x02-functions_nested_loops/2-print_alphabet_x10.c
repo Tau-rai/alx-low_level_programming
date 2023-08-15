fbe391b062f6647bbbece970694963a8f5dc3a3d
Taurai <taumasaire@gmail.com>
2023-08-15 13:40:41 +0200

    print alpha x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabet in lower case x10
+ *
+ * Return: Nothing
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 10;
+
+	do {
+		char alpha = 'a';
+
+		while (alpha <= 'z')
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+		_putchar('\n');
+	} while (i <= 10);
+}
