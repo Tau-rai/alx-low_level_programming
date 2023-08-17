f9074d491d8fa00ee7e2044f91ccc132c6972e13
Taurai <taumasaire@gmail.com>
2023-08-17 13:30:29 +0200

    task 3 print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * print_numbers - prints the numbers 0 through 9
+ *
+ * Return: 1 Success
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		_putchar(i + '0');
+		i++;
+	}
+	_putchar('\n');
+}
