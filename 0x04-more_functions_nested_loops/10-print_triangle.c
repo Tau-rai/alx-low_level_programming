004fd5a5c0b1098f79917f1a1046a84dca698680
Taurai <taumasaire@gmail.com>
2023-08-18 12:56:55 +0200

    seven correction

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,20 +7,25 @@
  */
 void print_triangle(int size)
 {
-	int n;
+	int n, m;
 
-	for (n = 1; n <= size; n++)
+	if (size <= 0)
 	{
-		int m;
-
-		for (m = 1; m <= size - n; m++)
-		{
-			_putchar(' ');
-		}
-		for (m = 1; m <= n; m++)
-		{
-			_putchar('#');
-		}
 		_putchar('\n');
+	}
+	else
+	{
+		for (n = 1; n <= size; n++)
+		{
+			for (m = 1; m <= size - n; m++)
+			{
+				_putchar(' ');
+			}
+			for (m = 1; m <= n; m++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
 }
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,23 +10,17 @@
 
 void print_diagonal(int n)
 {
-	int k;
+	int k, j;
 
+	if (n >= 0)
+		_putchar('\n');
 	for (k = 0; k < n; k++)
 	{
-		int j;
-
-		for (j = 0; j < n; j++)
+		for (j = 0; j < k; j++)
 		{
-			if (k == j)
-			{
-				_putchar('\\');
-			}
-			else
-			{
-				_putchar(' ');
-			}
+			_putchar(' ');
 		}
+		_putchar('\\');
 		_putchar('\n');
 	}
 }
