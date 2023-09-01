600423aae23e1c60c3dbdd68942fe0183207883c
Taurai <taumasaire@gmail.com>
2023-09-01 14:36:11 +0200

    task three correction

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,18 +10,18 @@
 int main(int argc, char *argv[])
 {
 	int i = 0;
-	int sum = 1;
+	int mul = 1;
 
-	if (argc == 1)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 	for (i = 1; i < argc; i++)
 	{
-		sum *= atoi(argv[i]);
+		mul *= atoi(argv[i]);
 	}
-	printf("%d\n", sum);
+	printf("%d\n", mul);
 
 	return (0);
 }
