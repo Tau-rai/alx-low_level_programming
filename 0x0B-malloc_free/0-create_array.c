02d98cc2713a37ca9d3fe45076291b244c5d7366
Taurai <taumasaire@gmail.com>
2023-09-04 16:34:03 +0200

    tasks zero and one

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * create_array - creates an array of chars
+ * @size: size of the array
+ * @c: initial value of char
+ * Return: Always 0.
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *arr = malloc(size * sizeof(char));
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			arr[i] = c;
+		}
+		return (arr);
+	}
+	return (0);
+}
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - duplicates a string
+ * @str: pointer to a string
+ * Return: Always 0.
+ */
+char *_strdup(char *str)
+{
+	return (strdup(str));
+}
