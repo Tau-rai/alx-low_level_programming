c2ea91f8a6e65df791499b05f9996e75cc4be0bc
Taurai <taumasaire@gmail.com>
2023-09-29 22:24:02 +0200

    little endian

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ * Return: 0 if big endian or 1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned int k = 1;
+	char *c = (char *)&k;
+
+	return ((int)*c);
+}
