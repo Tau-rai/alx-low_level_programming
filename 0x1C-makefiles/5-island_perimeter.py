7b87df8ee4d94ffee301a091454fbb22ebcdda66
Tau <taumasaire@gmail.com>
2024-01-24 19:11:08 +0200

    island perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This module contains a function that computes the perimeter an an island
+"""
+
+
+def island_perimeter(grid):
+    """Calculates the perimeter of an island
+
+    Args:
+        island (list): a list of integers
+    """
+    rows, cols = len(grid), len(grid[0])
+    perimeter = 0
+
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                if row == 0 or grid[row - 1][col] == 0:
+                    perimeter += 1
+                if row == rows - 1 or grid[row + 1][col] == 0:
+                    perimeter += 1
+                if col == 0 or grid[row][col - 1] == 0:
+                    perimeter += 1
+                if col == cols - 1 or grid[row][col + 1] == 0:
+                    perimeter += 1
+
+    return perimeter
